8bf26c4f62d54fee3f64d435e7098bf0e03b0950
Noémie ROCHARD <nrochard@student.42.fr>
2020-11-25 21:08:52 +0100

    info ok for anime and manga

--- js/app.js
+++ js/app.js
@@ -4,8 +4,6 @@ document.querySelector(".form").addEventListener("submit", (e) => {
 
     let input = document.querySelector(".input").value;
     let type = document.querySelector("#type").shadowRoot.querySelector(".select-text").innerHTML;
-  
-    console.log(type);
 
     //Gestion d'erreur des types de résultat
     let typeResult;
@@ -44,37 +42,55 @@ document.querySelector(".form").addEventListener("submit", (e) => {
 })
 
 function requestToApi(input, typeResult, type){
-    console.log("input pour API ->", input);
-    console.log("type pour API ->", typeResult);
-  
+
+    let slider = document.querySelector("ion-slides");
+    let nbResult = document.querySelector(".nbResult");
 
     axios
     .get(`https://api.jikan.moe/v3/search/${typeResult}?q=${input}&limit=10`)
         .then((response) =>{
-            // console.log(response.data);
-            console.log("type pour API ->", typeResult);
+            
+            //Gestion d'erreur si pas de résultat de l'API
+            if(response.data.results.length == 0){
+                displayError("No result, please try another thing !");   
+            }
+
+            //Affichage du nombre de résultat
+            response.data.results.length > 1 ? nbResult.innerHTML = `${ response.data.results.length} results` : `${ response.data.results.length} result`
+
             if (typeResult === "anime" || typeResult === "manga"){
                 let i = 0;
                 let nbSlide = 1;
                 response.data.results.forEach(element => {
+
+                    //Modification de la date au format anglais plus propre
+                    let newDate = formatDate(element.start_date);
+    
+                    //Reset du contenu
                     let card = document.querySelector(`.slide${nbSlide}`);
                     card.innerHTML="";
+
+                    //Insertion des résultats dans le slider
                     card.insertAdjacentHTML("afterbegin", `
                     <ion-card color="light">
                         <ion-card-header>
-                            <ion-card-subtitle>${type}</ion-card-subtitle>
-                            <img src="${element.image_url}">
+                            <ion-card-subtitle>${type} (${element.type})</ion-card-subtitle>
                             <ion-card-title class="title ion-padding">${element.title}</ion-card-title>
+                            <img src="${element.image_url}">
                         </ion-card-header>
                         <ion-card-content>
-                            <p class="synopsis${i}">${element.synopsis}</p>
-                            <a href="${element.url}">En savoir plus</a>
+                            <p class="align_synopsis created_at">Created at : ${newDate}</p>
+                            <p class="align_synopsis synopsis${i}">${element.synopsis}</p>
+                            <a class="link" target="_blank" href="${element.url}">En savoir plus</a>
                         </ion-card-content>
                     </ion-card>
                 `)
+
+                    //Gestion du nombre d'épisodes ou de chapitre
                     let moreInfo = document.querySelector(`.synopsis${i}`);
-                    typeResult === "anime" ? moreInfo.insertAdjacentHTML("afterend", `<p>Nombre d'épisodes : ${element.episodes} </p>`) : 
-                    moreInfo.insertAdjacentHTML("afterend", `<p>Nombre de chapitres : ${element.chapters} </p>`) 
+                    typeResult === "anime" ? moreInfo.insertAdjacentHTML("afterend", `<p class="chapter_episode">Number of episode(s) : ${element.episodes} </p>`) : 
+                    moreInfo.insertAdjacentHTML("afterend", `<p class="chapter_episode">Number of chapter(s) : ${element.chapters} </p>`) 
+
                     i++;
                     nbSlide++;
                 });
@@ -82,6 +98,7 @@ function requestToApi(input, typeResult, type){
             else{
                 let nbSlide = 1;
                 response.data.results.forEach(element => {
+                    console.log(element);
                     let card = document.querySelector(`.slide${nbSlide}`);
                     card.innerHTML="";
                     card.insertAdjacentHTML("afterbegin", `
@@ -92,6 +109,7 @@ function requestToApi(input, typeResult, type){
                             <ion-card-title class="title ion-padding">${element.name}</ion-card-title>
                         </ion-card-header>
                     </ion-card>`)
+                    nbSlide++;
                 });
             }
 
@@ -99,12 +117,12 @@ function requestToApi(input, typeResult, type){
     .catch((error) => {
         if(error.response){
             displayError("Il y a eu un problème, veuillez réessayer plus tard");
-            // console.log(error.response.data);
         }
     });
     
 }
 
+// Fonction d'affichage des erreurs
 async function displayError(messageError) {
     const toast = document.createElement('ion-toast');
     toast.message = messageError;
@@ -128,11 +146,25 @@ async function displayError(messageError) {
     console.log('Loading dismissed!');
   }
 
-  var slides = document.querySelector('ion-slides');
+// Sliders des résultats
+var slides = document.querySelector('ion-slides');
 
-// Optional parameters to pass to the swiper instance.
-// See http://idangero.us/swiper/api/ for valid options.
-slides.options = {
+    slides.options = {
   initialSlide: 1,
   speed: 400
+}
+
+// Fonction pour formater la date
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [year, month, day].join('-');
 }
